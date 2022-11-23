55542768d264268130fbdf5f7a77e6e88bd09637
unknown <Administrator@CADSYS.SGLIGIS.COM>
2022-11-23 10:46:45 +0530

    project added

---
+++ testDialogBasedApplication/testDialogBasedApplication.h
@@ -0,0 +1,32 @@
+
+// testDialogBasedApplication.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CtestDialogBasedApplicationApp:
+// See testDialogBasedApplication.cpp for the implementation of this class
+//
+
+class CtestDialogBasedApplicationApp : public CWinApp
+{
+public:
+	CtestDialogBasedApplicationApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CtestDialogBasedApplicationApp theApp;
